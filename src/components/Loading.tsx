5ff872a988dd3fbd422cf30257bf10d1c0110e86
cristianireyes <cristianir.dev@gmail.com>
2021-11-18 14:17:02 -0300

    Se centra Loading en View

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { View } from 'react-native';
 import { ActivityIndicator, Colors } from 'react-native-paper';
 
-const Loading = () => <ActivityIndicator animating={true} color={Colors.red700} />;
+const Loading = () => (
+  <View style={{ flex: 1, justifyContent: 'center' }}>
+    <ActivityIndicator animating={true} color={Colors.red700} />
+  </View>
+);
 
 export default Loading;
