6484d2df17124263f6de19af11b2fe79a88e1ac4
cristianireyes <cristianir.dev@gmail.com>
2021-11-18 21:32:45 -0300

    Ajustes menores en declaracion de estilos y tipos

--- src/components/Weather/WeatherIcon.tsx
+++ src/components/Weather/WeatherIcon.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { Avatar } from 'react-native-paper';
-
-const styles = StyleSheet.create({
-  icon: {
-    backgroundColor: 'transparent',
-  },
-});
 
 const WeatherIcon = ({ id, size = 40 }: { id: string; size?: number }) => (
   <Avatar.Image
-    style={styles.icon}
+    style={{ backgroundColor: 'transparent' }}
     size={size}
     source={{ uri: `http://openweathermap.org/img/wn/${id}@2x.png` }}
   />
